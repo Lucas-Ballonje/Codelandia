95f2e1cd07cd06fd65ec54fb8d7e47edbb439927
Lucas-Ballonje <lucas.ballonje@ativo.com>
2022-06-15 17:45:51 -0300

    fix: add JS e ajustes finos css

---
+++ Desafio 4/js/main.js
@@ -0,0 +1,9 @@
+const verificaEmail = document.querySelector(".emailInput");
+
+const verificaSenha = document.querySelector(".senhaInput");
+
+const entrar = document.querySelector(".entrar");
+
+function Redirecionar(){
+    alert("redirecionando")
+}
