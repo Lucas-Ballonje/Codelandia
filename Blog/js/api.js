3fc3b0ccac3a543852be3c5c2d75558247ca04d4
LiL-14 <lucasballonje12@gmail.com>
2023-06-20 01:41:20 -0300

    fix :bug: ajuste no JS e finalizando a API com titulo e descricao

--- Blog/js/api.js
+++ Blog/js/api.js
@@ -1,13 +1,36 @@
-const tituloNoticia = document.querySelector(".tituloNoticia")
-const descricaoNoticia = document.querySelector(".descricaoNoticia")
+const tituloNoticia = document.querySelectorAll(".tituloNoticia")
+const descricaoNoticia = document.querySelectorAll(".descricaoNoticia")
 
-let searchNoticia = 1;
+let searchNoticia = []
 
-const fetchNoticia = async (noticia) => {
-    const APIResponse = await fetch(`https://newsapi.org/v2/top-headlines/sources?&apiKey=3e18ef60908e46a192efb8d4388d88d0`)
+const fetchNoticia = async () => {
+    const APIResponse = await fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=3e18ef60908e46a192efb8d4388d88d0`)
 
     if(APIResponse.status == 200){
-        const data = await APIResponse.json()
-        return data        
+        const data = await APIResponse.json();                
+        return data;
     }
 }
+
+const renderNoticia = async () => {
+    tituloNoticia.innerHTML = "Loading...";
+    descricaoNoticia.innerHTML = "Loading...";
+
+    const data = await fetchNoticia()
+    if(data){
+        data.articles.forEach((article, index) => {
+            if(tituloNoticia[index] && descricaoNoticia[index]){
+                tituloNoticia[index].textContent = article.title;
+                descricaoNoticia[index].textContent = article.description;
+            }
+        });
+    }else {
+        tituloNoticia.forEach((titulo) => (titulo.textContent = "Not Found"));
+        descricaoNoticia.forEach((descricao) => {
+            descricao.textContent = "Not Found"
+        })
+    }       
+}
+
+console.log(descricaoNoticia);
+renderNoticia()
